3b580fa3865841e315289d30829d9533ae755bd2
Josh Feierman <josh@sqljosh.com>
2023-06-24 09:12:39 -0400

    wip: dependency block generation

--- internal/terragrunt/terragrunt.go
+++ internal/terragrunt/terragrunt.go
@@ -5,22 +5,44 @@ amount of transitive dependencies that would introduce to the project.
 */
 package terragrunt
 
+import "github.com/zclconf/go-cty/cty"
+
 // TerragruntConfig is a complete Terragrunt configuration file, and seeks to mirror
-// https://pkg.go.dev/github.com/gruntwork-io/terragrunt@v0.40.0/config#TerragruntConfig.
+// https://pkg.go.dev/github.com/gruntwork-io/terragrunt@v0.40.2/config#TerragruntConfig.
 type TerragruntConfig struct {
-	Terraform TerraformConfig `hcl:"terraform,block"`
+	Terraform      TerraformConfig `hcl:"terraform,block"`
 	IncludeConfigs []IncludeConfig `hcl:"include,block"`
 }
 
 // TerraformConfig represents the `terraform` block in a Terragrunt configuration.
-// Mirrors: https://pkg.go.dev/github.com/gruntwork-io/terragrunt@v0.40.0/config#TerraformConfig
+// Mirrors: https://pkg.go.dev/github.com/gruntwork-io/terragrunt@v0.40.2/config#TerraformConfig
 type TerraformConfig struct {
 	Source string `hcl:"source,attr"`
 }
 
 type IncludeConfig struct {
-	Name string `hcl:"name,label"`
-	Path string `hcl:"path,attr"`
-	Expose *bool `hcl:"expose,attr"`
+	Name          string  `hcl:"name,label"`
+	Path          string  `hcl:"path,attr"`
+	Expose        *bool   `hcl:"expose,attr"`
 	MergeStrategy *string `hcl:"merge_strategy,attr"`
 }
+
+// Dependency represents the `dependency` block in a Terragrunt configuration.
+// Mirrors: https://pkg.go.dev/github.com/gruntwork-io/terragrunt@v0.40.2/config#Dependency
+type Dependency struct {
+	Name                                string     `hcl:",label"`
+	ConfigPath                          string     `hcl:"config_path,attr"`
+	SkipOutputs                         *bool      `hcl:"skip_outputs,attr"`
+	MockOutputs                         *cty.Value `hcl:"mock_outputs,attr"`
+	MockOutputsAllowedTerraformCommands *[]string  `hcl:"mock_outputs_allowed_terraform_commands,attr"`
+	MockOutputsMergeStrategyWithState		*MergeStrategyType `hcl:"mock_outputs_merge_strategy_with_state"`
+}
+
+type MergeStrategyType string
+
+const (
+	NoMerge          MergeStrategyType = "no_merge"
+	ShallowMerge     MergeStrategyType = "shallow"
+	DeepMerge        MergeStrategyType = "deep"
+	DeepMergeMapOnly MergeStrategyType = "deep_map_only"
+)
